88078f824e1cb0e3e08007e5358bf9721109204c
Buddhsen TRIPATHI <buddhsen.tripathi@amadeus.com>
2023-04-07 13:20:15 +0530

    Added Footer Component

--- betteromagle/src/app/app.module.ts
+++ betteromagle/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
